51c7489fb2c4cb53f07176b94fa22238e39a6312
Artix1500 <artur.haczek23@gmail.com>
2019-04-01 13:45:24 +0200

    Fix for reading pdf's

--- loadData.py
+++ loadData.py
@@ -1,14 +1,31 @@
 import glob
-import textract
-
+import PyPDF2
 
 def load_pdfs():
     root_dir = 'data'
     files = []
     for filename in glob.iglob(root_dir + '**/**/*.pdf', recursive=True):
+
+        # creating an object
+        file = open(filename, 'rb')
+
+        # creating a pdf reader object
+        fileReader = PyPDF2.PdfFileReader(file)
+
+        # print the number of pages in pdf file
         print(filename)
-        text = textract.process(filename)
+        print(fileReader.numPages)
+        num_pages = fileReader.numPages
+        count = 0
+        text = ""
+        # The while loop will read each page
+        while count < num_pages:
+            pageObj = fileReader.getPage(count)
+            count += 1
+            text += pageObj.extractText()
+
         print(text)
 
 
-load_pdfs()
+if __name__ == "__main__":
+    load_pdfs()
