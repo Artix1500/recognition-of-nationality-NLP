08498b0c01e7c88eea2b30fa7fcd65a1fecf0978
Grzegorz_Kacprowicz <kacprowiczg@student.mini.pw.edu.pl>
2019-04-01 14:54:59 +0200

    Sketch

--- loadData.py
+++ loadData.py
@@ -10,6 +10,11 @@ def processText(text):
             dictionary[word] = 1
         else:
             dictionary[word] += 1
+
+    key = 'the'
+    # print(max(dictionary, key=dictionary.get))
+    if key in dictionary:
+        print(dictionary[key])
     return dictionary
 
 
@@ -26,7 +31,7 @@ def load_pdfs():
 
         # print the number of pages in pdf file
         print(filename)
-        print(fileReader.numPages)
+        # print(fileReader.numPages)
         num_pages = fileReader.numPages
         count = 0
         text = ""
@@ -37,8 +42,18 @@ def load_pdfs():
             text += pageObj.extractText()
 
         dict = processText(text)
-        print(dict)
-
+        # new string (line for data.csv)
+        # read lines from dictionary.txt
+        # for each word
+        #   append to string number and comma
+        #   remove this key from dict
+        # if dict is not empty after removing known words start updating CSV file
+        # For each key in dict
+        #   add column to CSV file and put 0 in each line
+        #   append number to string and comma
+        # after updating CSV append our string to the end of data.CSV
+        # print(dict)
+#
 
 if __name__ == "__main__":
     load_pdfs()
