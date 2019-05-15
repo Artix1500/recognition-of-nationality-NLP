24406bcdc15e48473a315eb672be12e71a21544c
Karolina <swietlny.rycerz@gmail.com>
2019-05-15 15:37:46 +0200

    cleanings and lemmatization using

---
+++ preprocessing/ClearData.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from lemmatization import dataLemmatization
+
+def ClearData(pathFrom="data.csv", pathTo="ProcessedData.csv", withLemmatization=False):
+    df = pd.DataFrame()
+
+    print("Reading data")
+    data = df
+    for chunk in pd.read_csv(pathFrom,  chunksize=100):
+        data = pd.concat([data, chunk])
+    print(data)
+
+s
+    if withLemmatization:
+        print("Lemmatization")
+        data = dataLemmatization(data)
+
+    print("dropping first column")
+    data = data.drop("Unnamed: 0", axis=1)
+
+    print("summing columns")
+    summed_colums = data.sum(axis=0, skipna=True)
+
+    print("dropping columns")
+    minCount = 10
+    data = data[data.columns[data.sum() > minCount]]
+
+    print("saving to file")
+    data.to_csv(pathTo)
+
+if __name__ == '__main__':
+    ClearData(withLemmatization=True)
--- preprocessing/lemmatization.py
+++ preprocessing/lemmatization.py
@@ -2,6 +2,8 @@ import pandas as pd
 from nltk.stem import WordNetLemmatizer
 
 # Przyjmuje i zwraca pandas.DataFrame z kolumnami poetykietowanymi słowami
+# WAZNE: urzywa (pierwsza kolumna)Unnamed: 0  jako slowa, trzeba to zmienic
+# i pewnie tez word count :<<<<<<<<<<<
 
 def dataLemmatization(data):
     newWords = []
@@ -9,12 +11,15 @@ def dataLemmatization(data):
     newQuantityDF = pd.DataFrame()
     lemmatizer = WordNetLemmatizer()
     for word in allWords:
+        print("New Word "+word)
         lemmatizedWord = lemmatizer.lemmatize(word)
+        print("lemmatized word " + word)
         if lemmatizedWord in newWords:
+            print("is already in set")
             newQuantityDF[lemmatizedWord] += data[word]
         else:
+            print("is not")
             newWords.append(lemmatizedWord)
             newCol = data[word]
             newQuantityDF = pd.concat([newQuantityDF, pd.DataFrame({lemmatizedWord:newCol})], axis=1)
-
     return newQuantityDF
