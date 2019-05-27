5ab3184d8ff9f75d151e8dcf2106293e54250f91
fikrys <k.filipiak1997@gmail.com>
2019-05-28 00:29:30 +0200

    Push dla Grzesia

---
+++ raw_text_processor.py
@@ -0,0 +1,51 @@
+import re
+
+
+def join_hyphenated_words(raw_text):
+    result = []
+    previous_word = ''
+    for raw_word in raw_text.split():
+        word = previous_word + raw_word.strip()
+        previous_word = ''
+        if word[-1] == '-':
+            previous_word = word[:-1]
+            continue
+        result.append(word)
+    return ' '.join(result)
+
+
+def delete_regex_from_text(regex, raw_text):
+    return "".join(re.split(regex, raw_text))
+
+
+def delete_square_brackets(raw_text):
+    return delete_regex_from_text('\[[0-9]*\]', raw_text)
+
+
+def split_by(raw_text, delimiter):
+    splitted_text = raw_text.split(delimiter)
+    splitted_text_with_delimiter = [text + delimiter for text in splitted_text[:-1]]
+    if len(splitted_text[-1]) > 0:
+        splitted_text_with_delimiter.append(splitted_text[-1])
+    return splitted_text_with_delimiter
+
+
+def split_list_by(raw_list, delimiter):
+    result = []
+    for sentence in raw_list:
+        result.extend(split_by(sentence, delimiter))
+    return result
+
+
+def split_to_sentences(raw_text):
+    exclamatory_sentences_split = split_by(raw_text, '!')
+    interrogative_sentences_split = split_list_by(exclamatory_sentences_split, '?')
+    declarative_sentences_split = split_list_by(interrogative_sentences_split, '.')
+
+    sentences_list = []
+    for sentence in declarative_sentences_split:
+        if sentence[0] is ' ' and sentence[1].isupper():
+            sentences_list.append(sentence)
+        elif len(sentences_list) > 0:
+            sentences_list[-1] += sentence
+    return sentences_list
---
+++ sentences_processor.py
@@ -0,0 +1,122 @@
+import re
+
+
+def create_dictionary(raw_list):
+    dictionary = {}
+    for element in raw_list:
+        dictionary[element] = dictionary.get(element, 0) + 1
+    return dictionary
+
+
+def delete_footers_and_headers(sentences_list):
+    return [k for k, v in create_dictionary(sentences_list).items() if v < 2]
+
+
+def sentence_statistic(sentence):
+    length = len(sentence)
+    non_ascii_characters = 0
+    small_letters = 0
+    big_letters = 0
+    digits = 0
+    other_ascii_characters = 0
+
+    for char in sentence:
+        if ord(char) > 127:
+            non_ascii_characters += 1
+        elif char.islower():
+            small_letters += 1
+        elif char.isupper():
+            big_letters += 1
+        elif char.isdigit():
+            digits += 1
+        else:
+            other_ascii_characters += 1
+    '''
+    statistic = namedtuple('statistic', [
+        'length',
+        'non_ASCII_characters',
+        'small_letters',
+        'big_letters',
+        'digits',
+        'other_ASCII_characters'])
+    '''
+    return [length, 100*non_ascii_characters/length, 100*small_letters/length, 100*big_letters/length, 100*digits/length, 100*other_ascii_characters/length]
+
+
+def aggressive_sentence_detector(sentence):
+    stat = sentence_statistic(sentence)
+    if stat[0] < 25:
+        return False
+    if stat[2] < 50:
+        return False
+    if stat[3] > 10:
+        return False
+    if stat[4] > 5:
+        return False
+    if stat[5] > 30:
+        return False
+    return True
+
+
+def references_detector(sentence):
+    stat = sentence_statistic(sentence[:30])
+    return stat[5] * stat[0] / 100 > 5
+
+
+def sentence_verifier(sentence):
+    return aggressive_sentence_detector(sentence) and not references_detector(sentence)
+
+
+def select_correct_sentences(sentences_list, is_sentence_verifier):
+    return [sentence for sentence in sentences_list if is_sentence_verifier(sentence)]
+
+
+def print_sentences(sentences_list):
+    for sentence in sentences_list:
+        print("#" * 80, sentence, sep="\n")
+
+
+if __name__ == '__main__':
+    import raw_text_processor as rtproc
+    from tika import parser
+
+    pdf_path = 'data/Polish/BlochoNalepa.pdf'
+    pdf_text = parser.from_file(pdf_path)
+    text = pdf_text['content']
+    text = rtproc.join_hyphenated_words(text)
+    text = rtproc.delete_square_brackets(text)
+    sent = rtproc.split_to_sentences(text)
+    sent = select_correct_sentences(sent, sentence_verifier)
+    print_sentences(sent)
+
+
+    from ner import properNounsOut
+
+    def NER(sentences_list):
+        processed_text = " ".join(sentences_list)
+        words = properNounsOut(processed_text)
+        return words
+
+
+    def print_dictionary(dic):
+        for k, v in dic.items():
+            print(k, v)
+
+    #final = NER(sent)
+    #print_dictionary(final)
+
+
+
+
+    '''
+    sentences_list = split_to_sentences(join_hyphenated_words(pdf_text['content']))
+    old_number_of_sentences = len(sentences_list)
+    sentences_list = delete_sentences_with_many_numbers(sentences_list)
+    sentences_list = deleteWhitespacesOnEdges(sentences_list)
+    sentences_list = delete_footers_and_headers(sentences_list)
+    map(delete_square_brackets, sentences_list)
+    words = NER(sentences_list)
+    words = filter_words(words)
+    final_dict = create_dictionary(words)
+    print_dictionary(final_dict)
+    '''
