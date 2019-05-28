4b40da27cfb2bc337fcb0e76f4bf917f751efcf0
Grzegorz_Kacprowicz <kacprowiczg@student.mini.pw.edu.pl>
2019-05-28 02:15:06 +0200

    Add comments to loadData.py, delete two unused files

--- loadData.py
+++ loadData.py
@@ -1,34 +1,27 @@
 import glob
 import sys
-import string
-
 import os
 import numpy as np
 import codecs
-import textprocessing as tp
-import ner
 from tika import parser
 from sentences_processor import process_PDF_content
 
 np.set_printoptions(threshold=sys.maxsize)
 
+# logging variables
 FileCount = 0
 LineCount = 0
 
 
-def processText(text):
-    return tp.create_dictionary(tp.split_to_words(text))
-
-
+# read data from csv file and save it into python variables using lists and numpy.ndarray
 def splitData(csvContent):
     filenamesInFile = []
     quantitiesList = []
 
-    # assuming there is at least one line ended in \n
-
+    # we assume there is at least one line ending with \n
     index = csvContent.index("\n") if "\n" in csvContent else -1
     existingWords = [] if index == -1 else csvContent[:index].split(',')[1:]
-    # print('ex:' + str(len(existingWords)))
+    # read all lines of data.csv and extract values between commas
     for line in csvContent.split('\n')[1:]:
         row = line.strip().split(',')
         filenamesInFile.append(row[0])
@@ -38,6 +31,7 @@ def splitData(csvContent):
     return existingWords, filenamesInFile, wordQuantitiesMatrix
 
 
+# search for indexes of rows to be deleted, keeping the right order
 def getIndexesOfRowsToBeDeleted(filenamesInFile, rowsToBeDeleted):
     indexes = []
     for filename in rowsToBeDeleted:
@@ -46,13 +40,14 @@ def getIndexesOfRowsToBeDeleted(filenamesInFile, rowsToBeDeleted):
     return indexes
 
 
-# probably unnecessary to delete fileLines
+# delete rows of files no longer found in data/ folder
 def deleteRows(indexesOfRowsToBeDeleted, wordQuantitiesMatrix):
     for index in indexesOfRowsToBeDeleted[::-1]:
         wordQuantitiesMatrix = np.delete(wordQuantitiesMatrix, index, axis=0)
     return wordQuantitiesMatrix
 
 
+# create dictionary (pdf_filename: processed words of this pdf)
 def readPDFsFromNewFiles(rowsToBeAdded):
     # fileName: its content
     newFilesDicts = {}
@@ -61,9 +56,8 @@ def readPDFsFromNewFiles(rowsToBeAdded):
         try:
             FileCount = FileCount + 1
             text = parser.from_file(newFile)
-            # newFilesDicts[newFile] = processText(text['content'])
+            print("PFD number: " + str(FileCount) + ') ' + str(newFile) + " is being processed")
             newFilesDicts[newFile] = process_PDF_content(text['content'])
-            print(str(FileCount) + '\t')
         except KeyError:
             print("KEYERROR in " + newFile)
         except UnicodeEncodeError:
@@ -74,13 +68,14 @@ def readPDFsFromNewFiles(rowsToBeAdded):
     return newFilesDicts
 
 
+# enlarge matrix containing quantities of words, extend list of all words
 def addNewRows(newFilesDicts, existingWords, wordQuantitiesMatrix):
     for filename, allwords in newFilesDicts.items():
-        # concatenation of strings in python makes new object, thus we have array for now
         newWords = []
         newRowQuantities = []
         existingWordsQuantities = {}
         oldWordsLength = len(existingWords)
+        # create dictionary/update quantity of word
         for word, quantity in allwords.items():
             if word not in existingWords:
                 newWords.append(word)
@@ -90,14 +85,16 @@ def addNewRows(newFilesDicts, existingWords, wordQuantitiesMatrix):
 
         existingWords.extend(newWords)
 
+        # make new bigger matrix with zeros, then put old matrix into the bigger one
         newMatrix = np.zeros((wordQuantitiesMatrix.shape[0] + 1, len(existingWords)), dtype="uint16")
         if newWords:
-            print(filename + " is being processed")
+            print(filename + " added " + str(len(existingWords) - oldWordsLength) + " new words")
             newMatrix[:-1, :-len(newWords)] = wordQuantitiesMatrix
         else:
-            print("! " + filename + " DID NOT add any new words")
+            print(filename + " DID NOT add any new words")
             newMatrix[:-1, :] = wordQuantitiesMatrix
 
+        # add quantities of the words in new PDF row
         for k, v in existingWordsQuantities.items():
             newMatrix[-1, existingWords.index(k)] = v
 
@@ -106,6 +103,7 @@ def addNewRows(newFilesDicts, existingWords, wordQuantitiesMatrix):
     return existingWords, wordQuantitiesMatrix
 
 
+# delete zero columns (after deleting some pdf from data/ folder)
 def removeZeroColumns(existingWords, wordQuantitiesMatrix):
     print("Removing columns...")
     indexesToDelete = list(np.argwhere(np.all(wordQuantitiesMatrix[..., :] == 0, axis=0)).flat)
@@ -125,13 +123,16 @@ def removeZeroColumns(existingWords, wordQuantitiesMatrix):
     return existingWords, wordQuantitiesMatrix
 
 
+# creates rows to save to file in such manner: path,item1_quantity,item2_quantity...
 def updateCSV(existingWords, filenamesSearched, wordQuantitiesMatrix, pathToCsv):
     rows = []
+    # create first row
     firstRow = ',' + ','.join(existingWords)
     rows.append(firstRow)
     global LineCount
+    # create rows from collected data for each file
     for i in range(wordQuantitiesMatrix.shape[0]):
-        print("SAVE the: " + str(LineCount) + " line to rows var")
+        print("Append line nr " + str(LineCount))
         LineCount = LineCount + 1
         quantities = ','.join(map(str, wordQuantitiesMatrix.astype(int)[i, :].tolist()))
         rows.append(filenamesSearched[i] + ',' + quantities)
@@ -144,24 +145,29 @@ def updateCSV(existingWords, filenamesSearched, wordQuantitiesMatrix, pathToCsv)
         wordQuantitiesMatrix.shape[0]) + " files")
 
 
+# loads csv file, searches for new files and updates data.csv file with dictionary and word quantities
 def updateData():
     root_dir = 'data'
     pathToCsv = os.path.join(root_dir, 'data.csv')
     with codecs.open(pathToCsv, 'r+', 'utf-8') as csvFile:
         csvContent = csvFile.read().strip()
 
+    # read data from csv file and save it into python variables
     existingWords, filenamesInFile, wordQuantitiesMatrix = splitData(csvContent)
+    # search for pdf files to add
     filenamesSearched = list(glob.iglob(root_dir + '**/**/*.pdf', recursive=True))
+    # sort filenames to keep the order of files ant to not modify them in csv file
     filenamesSearched.sort()
     rowsToBeDeleted = list(set(filenamesInFile) - set(filenamesSearched))
     rowsToBeDeleted.sort()
     rowsToBeAdded = list(set(filenamesSearched) - set(filenamesInFile))
     rowsToBeAdded.sort()
 
-    print("\nFiles found in filesystem: " + str(filenamesSearched))
-    print("Files to be added to CSV: " + str(rowsToBeAdded))
-    print("Files to be deleted from CSV: " + str(rowsToBeDeleted))
+    print("\nFiles found in filesystem: " + str(len(filenamesSearched)))
+    print("Files to be added to CSV: " + str(len(rowsToBeAdded)))
+    print("Files to be deleted from CSV: " + str(len(rowsToBeDeleted)))
 
+    # return early if no changes to PDFs in file system detected
     if not rowsToBeAdded and not rowsToBeDeleted:
         print("\nNothing to be changed")
         print(
@@ -169,23 +175,28 @@ def updateData():
                 wordQuantitiesMatrix.shape[0]) + " files")
         return
 
+    # delete old rows (files that no longer exist in data/ folder)
     indexesOfRowsToBeDeleted = getIndexesOfRowsToBeDeleted(filenamesInFile, rowsToBeDeleted)
     wordQuantitiesMatrix = deleteRows(indexesOfRowsToBeDeleted, wordQuantitiesMatrix)
+    # add new rows, enlarge word dictionary
     if rowsToBeAdded:
         print("Reading PDFs...")
         newFilesDicts = readPDFsFromNewFiles(rowsToBeAdded)
         existingWords, wordQuantitiesMatrix = addNewRows(newFilesDicts, existingWords, wordQuantitiesMatrix)
 
+    # delete potential zero columns (after deleting some pdf from data/ folder)
     if wordQuantitiesMatrix.size != 0 and rowsToBeDeleted.__len__() > 0:
         existingWords, wordQuantitiesMatrix = removeZeroColumns(existingWords, wordQuantitiesMatrix)
 
-    # to get the proper order of rows in CSV
+    # get the proper order of rows in CSV
     filenames = [x for x in filenamesInFile if x not in rowsToBeDeleted]
     filenames.extend(rowsToBeAdded)
 
+    # clear dictionary if there are no PDFs in data/
     if wordQuantitiesMatrix.size == 0:
         existingWords.clear()
 
+    # save/update data to data.csv file
     updateCSV(existingWords, filenames, wordQuantitiesMatrix, pathToCsv)
 
 
